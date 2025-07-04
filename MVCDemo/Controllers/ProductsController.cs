3bbb59dfb07d1d95b6c2d05ea2ca9bc8ebc90483
kuntesv <kuntesv@gmail.com>
2025-07-04 11:51:02 +0530

    products controller changed

--- MVCDemo/Controllers/ProductsController.cs
+++ MVCDemo/Controllers/ProductsController.cs
@@ -17,6 +17,7 @@ namespace MVCDemo.Controllers
 
         public IActionResult Add()
         {
+            ViewBag.Action = "add";
             var productViewModel = new ProductViewModel()
             {
                 Categories = CategoriesRepository.GetCategories()
@@ -35,7 +36,52 @@ namespace MVCDemo.Controllers
                 return RedirectToAction(nameof(Index));
             }
 
+            ViewBag.Action = "add";
+            productViewModel.Categories = CategoriesRepository.GetCategories();
+
             return View(productViewModel);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "edit";
+
+            var productViewModel = new ProductViewModel
+            {
+                Product = ProductsRepository.GetProductById(id) ?? new Product(),
+                Categories = CategoriesRepository.GetCategories()
+            };
+
+            return View(productViewModel);
+
+        }
+
+        [HttpPost]
+        public IActionResult Edit(ProductViewModel productViewModel)
+        {
+            if (ModelState.IsValid) {
+
+                ProductsRepository.UpdateProduct(productViewModel.Product.ProductId, productViewModel.Product);
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewBag.Action = "edit";
+
+            productViewModel.Categories = CategoriesRepository.GetCategories();
+            return View(productViewModel);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            ProductsRepository.DeleteProduct(id);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult ProductsByCategoryPartial(int categoryId)
+        {
+            var products = ProductsRepository.GetProductsByCategoryId(categoryId);
+            return PartialView("_Products", products);
         }
 
 
